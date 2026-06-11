9ef9437fbcbabe2162f27d1bc108524f7f486261
lucastsadv-commits <lucas.tsadv@gmail.com>
2026-06-11 17:26:15 -0300

    feat: wrap App component with TournamentProvider in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TournamentProvider } from './context/TournamentContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TournamentProvider>
+      <App />
+    </TournamentProvider>
   </StrictMode>,
 )
