219e0983d29c5c5944ed5490ccfbdacacf198bd2
lucastsadv-commits <lucas.tsadv@gmail.com>
2026-06-12 10:51:41 -0300

    Fix TS6133: Remove unused TournamentProvider import

--- src/components/TabelaGallery.tsx
+++ src/components/TabelaGallery.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import GroupStage from './GroupStage';
 import KnockoutBracket from './KnockoutBracket';
-import { TournamentProvider } from '../context/TournamentContext';
 
 const TabelaGallery: React.FC = () => {
   return (
